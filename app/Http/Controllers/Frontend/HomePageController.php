4d32a443af682834590c7b78dacf60249e0bf97d
billdevmaster71 <billdevmaster@gmail.com>
2023-03-01 15:31:56 +0900

    fix: update estonia time

--- app/Http/Controllers/Frontend/HomePageController.php
+++ app/Http/Controllers/Frontend/HomePageController.php
@@ -281,8 +281,10 @@ class HomePageController extends Controller
         $day = [];
         if ($date1 <= date_create("2022-10-30")) {
             $day['date'] = strtotime($request['start_date']) * 1 - 7200;
-        } else {
+        } else if ($date1 <= date_create("2023-03-27")) {
             $day['date'] = strtotime($request['start_date']) * 1 - 3600;
+        } else {
+            $day['date'] = strtotime($request['start_date']) * 1;
         }
         $day['openTimes'] = $this->getLocationOpenTimes($request['office'], $request['start_date']);
         $day['resources'] = $this->getLocationPesuboxs($request['office'], $request['start_date']);
